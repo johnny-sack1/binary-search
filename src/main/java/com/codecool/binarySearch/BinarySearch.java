6ec85e5e26324db734835eb55d11da6ea0550824
johnny-sack1 <jonymaka@gmail.com>
2018-10-13 12:06:58 +0200

    Implement algorithm and test

--- src/main/java/com/codecool/binarySearch/BinarySearch.java
+++ src/main/java/com/codecool/binarySearch/BinarySearch.java
@@ -1,4 +1,26 @@
 package com.codecool.binarySearch;
 
 public class BinarySearch {
+    public boolean search(int[] array, int searchedValue) {
+        int lowerBound = 0;
+        int upperBound = array.length - 1;
+
+        while (true) {
+            if (upperBound < lowerBound) {
+                return false;
+            }
+
+            int middle = lowerBound + (upperBound - lowerBound) / 2;
+
+            if (array[middle] < searchedValue) {
+                lowerBound = middle + 1;
+            }
+            if (array[middle] > searchedValue) {
+                upperBound = middle - 1;
+            }
+            if (array[middle] == searchedValue) {
+                return true;
+            }
+        }
+    }
 }
--- src/test/java/com/codecool/binarySearch/BinarySearchTest.java
+++ src/test/java/com/codecool/binarySearch/BinarySearchTest.java
@@ -1,4 +1,20 @@
 package com.codecool.binarySearch;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BinarySearchTest {
+    @Test
+    void searchIfPresentTest() {
+        BinarySearch finder = new BinarySearch();
+        int data[] = {10, 20, 31, 44, 47, 58, 99, 113, 119, 222};
+        assertTrue(finder.search(data, 113));
+    }
+
+    @Test
+    void searchIfAbsentTest() {
+        BinarySearch finder = new BinarySearch();
+        int data[] = {10, 20, 31, 44, 47, 58, 99, 113, 119, 222};
+        assertFalse(finder.search(data, 98));
+    }
 }
